82a6229221e3366ead6c5e337568eaf6703b0895
Antony Bartlett <antony.bartlett@ukri.org>
2022-08-27 18:23:01 +0100

    That might be roughly the algoritmn needed

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import checkmm, { Assertion, Expression } from 'checkmm';
 
 interface AssertionInfo {
+    labels: string[];
     hypothesesExpressionText: string[];
     disjvarText: string[];
     conclusionExpressionText: string;
@@ -11,67 +12,78 @@ interface LabelledAssertion {
     assertion: Assertion;
 }
 
-interface AssertionDuplicates {
-    labels: string[];
-    assertion: Assertion;
-}
-
-const getHypotheses = (a: Assertion): string[] => {
-    return a.hypotheses.map(label => checkmm.hypotheses.get(label)!.first.join(' '));
+const assertionsAreEqual = (a1: AssertionInfo, a2: AssertionInfo) => {
+    return (
+        a1.conclusionExpressionText === a2.conclusionExpressionText &&
+        checkmm.std.arraysequal(a1.hypothesesExpressionText, a2.hypothesesExpressionText) &&
+        checkmm.std.arraysequal(a1.disjvarText, a2.disjvarText)
+    );
 };
 
-const assertionsAreEqual = (a1: Assertion, a2: Assertion): boolean => {
-    // Compare expressions
-    const expressionsEqual = checkmm.std.arraysequal(a1.expression, a2.expression);
-    if (!expressionsEqual) {
-        return false;
-    }
+// const getHypotheses = (a: Assertion): string[] => {
+//     return a.hypotheses.map(label => checkmm.hypotheses.get(label)!.first.join(' '));
+// };
 
-    // Compare hypotheses
-    const hypothesesEqual = checkmm.std.arraysequal(getHypotheses(a1).sort(), getHypotheses(a2).sort());
-    if (!hypothesesEqual) {
-        return false;
-    }
+// const assertionsAreEqual = (a1: Assertion, a2: Assertion): boolean => {
+//     // Compare expressions
+//     const expressionsEqual = checkmm.std.arraysequal(a1.expression, a2.expression);
+//     if (!expressionsEqual) {
+//         return false;
+//     }
 
-    // Compare disjoint variables
-    const disjvars = new Set(a1.disjvars);
-    let missingDisjvar = false;
-    a2.disjvars.forEach(disjvar => {
-        missingDisjvar &&= disjvars.delete(disjvar);
-    });
+//     // Compare hypotheses
+//     const hypothesesEqual = checkmm.std.arraysequal(getHypotheses(a1).sort(), getHypotheses(a2).sort());
+//     if (!hypothesesEqual) {
+//         return false;
+//     }
 
-    if (missingDisjvar || disjvars.size) {
-        return false;
-    }
+//     // Compare disjoint variables
+//     const disjvars = new Set(a1.disjvars);
+//     let missingDisjvar = false;
+//     a2.disjvars.forEach(disjvar => {
+//         missingDisjvar &&= disjvars.delete(disjvar);
+//     });
 
-    return true;
-};
+//     if (missingDisjvar || disjvars.size) {
+//         return false;
+//     }
+
+//     return true;
+// };
 
 const assertionList: LabelledAssertion[] = [];
 
-const assertionMap = new Map<string, AssertionDuplicates[]>();
+const assertionMap = new Map<string, AssertionInfo[]>();
 
 const { constructassertion } = checkmm;
 
 checkmm.constructassertion = (label: string, expression: Expression) => {
     const assertion = constructassertion(label, expression);
-    const expressionText = expression.join(' ');
+    const conclusionExpressionText = expression.join(' ');
+    const assertionInfo: AssertionInfo = {
+        labels: [label],
+        hypothesesExpressionText: assertion.hypotheses.map(hyp => checkmm.hypotheses.get(hyp)!.first.join(' ')).sort(),
+        disjvarText: Array.from(assertion.disjvars)
+            .map(pair => [pair.first, pair.second].join(' '))
+            .sort(),
+        conclusionExpressionText,
+    };
 
     assertionList.push({ label, assertion });
-    if (!assertionMap.has(expressionText)) {
-        assertionMap.set(expressionText, []);
+    if (!assertionMap.has(conclusionExpressionText)) {
+        assertionMap.set(conclusionExpressionText, []);
     }
 
-    const assertionDuplicatesArray = assertionMap.get(expressionText)!;
+    const assertionInfoArray = assertionMap.get(conclusionExpressionText)!;
 
-    for (const assertionDuplicates of assertionDuplicatesArray) {
-        if (assertionsAreEqual(assertionDuplicates.assertion, assertion)) {
-            assertionDuplicates.labels.push(label);
+    for (const assertionInfoItem of assertionInfoArray) {
+        if (assertionsAreEqual(assertionInfoItem, assertionInfo)) {
+            assertionInfoItem.labels.push(label);
             return assertion;
         }
     }
 
-    assertionDuplicatesArray.push({ labels: [label], assertion });
+    assertionInfoArray.push(assertionInfo);
 
     return assertion;
 };
@@ -80,8 +92,8 @@ checkmm.main(process.argv.slice(1)).then(exitCode => {
     process.exitCode = exitCode;
 
     assertionList.forEach(labelledAssertion => {
-        const assertionDuplicatesArray = assertionMap.get(labelledAssertion.assertion.expression.join(' '));
-        console.log(JSON.stringify(assertionDuplicatesArray, null, 2));
+        const assertionInfoArray = assertionMap.get(labelledAssertion.assertion.expression.join(' '));
+        console.log(JSON.stringify(assertionInfoArray, null, 2));
         console.log();
-    })
+    });
 });
