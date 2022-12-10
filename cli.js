1d132ffcf4ca36e7dd4f816e7409e936bfdc9b28
Antony74 <akb@akb.me.uk>
2022-12-10 17:29:43 +0000

    Run as a command

---
+++ cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* global console process require */
+
+const child_process = require('child_process');
+const cmd = `npm start ${process.argv.slice(2)}`;
+const altmm = child_process.exec(cmd);
+
+altmm.stdout.on('data', data => {
+    console.log(data);
+});
